850eafa25e83e68870a0bb006a23ec7323d6df1f
sh-nishonov <nishonovshohjahon1@gmail.com>
2022-01-05 21:31:07 +0500

    Commit Custom Linear Regression

--- miniml/regression/MultiLinearRegression.py
+++ miniml/regression/MultiLinearRegression.py
@@ -1,72 +1,69 @@
 import numpy as np
 import copy
+import math
 
 
-class MultiLinearRegression():
-    def __init__(self):
-        self.coefficients = None
-        self.intercept = None
+class Regression():
+    def __init__(self, n_iterations, learning_rate):
+        #self.coefficients = None
+        #self.intercept = None
+        #self.gradient_descent = False
+        self.n_iterations = n_iterations
+        self.learning_rate = learning_rate
+        
+
+    
+        
 
     def fit(self, X, y):
-        X = self._transform_x(X)
-        y = self._transform_y(y)
+        # X = self._transform_x(X)
+        # y = self._transform_y(y)
 
-        betas = self._estimate_coefficients(X, y)
+        # betas = self._estimate_coefficients(X, y)
 
-        self.intercept = betas[0]
+        # self.intercept = betas[0]
 
-        self.coefficients = betas[1:]
+        # self.coefficients = betas[1:]
+        n_samples, n_features = X.shape
+        #X = np.insert(X, 0, 1, axis=1)
+        self.weights = np.random.uniform(-1 / math.sqrt(n_features), 1 / math.sqrt(n_features), (n_features, ))
+        self.bias = 0
+
+        for i in range(self.n_iterations):
+            y_hat = np.dot(X, self.weights) + self.bias # y_hat = X*W+b
+
+            dw = (1 / n_samples) * np.dot(X.T, (y_hat - y))
+            db = (1 / n_samples) * np.sum(y_hat - y)
+
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
 
     def predict(self, X):
         """
         y = b_0 + b_1*x + ... + b_i*x_i
         """
-        predictions = []
-        for index, row in X.iterrows():
-            values = row.values
-            pred = np.multiply(values, self.coefficients) # multiply argumets element-wise
-            pred = sum(pred)
-            pred += self.intercept
+        print(X)
+        #X = np.insert(X, 0, 1, axis=1)
+        print(X)
+        y_approximated = np.dot(X, self.weights)
+        return y_approximated
 
-            predictions.append(pred)
-
-        return predictions
-
-    def r2_score(self, y_true, y_pred):
-        '''
-            r2 = 1 - (rss/tss)
-            rss = sum_{i=0}^{n} (y_i - y_hat)^2
-            tss = sum_{i=0}^{n} (y_i - y_bar)^2
-        '''
-        y_values = y_true.values()
-        y_average = np.average(y_values)
-
-        residual_sum_of_square = 0
-        total_sum_of_squares = 0
-
-        for i in range(len(y_values)):
-            residual_sum_of_squares += (y_values[i] - y_pred[i]) ** 2
-            total_sum_of_squares += (y_values[i] - y_average[i]) ** 2
-
-        return 1 - (residual_sum_of_square / total_sum_of_squares)
-
-    def _transform_x(self, X):
-        X = copy.deepcopy(X)
-        X.insert(0, 'ones', np.ones((X.shape[0], 1)))
-        return X.values
-    
-    def _transform_y(self, y):
-        y = copy.deepcopy(y)
-        return y.values
-
-    def _estimate_coefficients(self, X, y):
-        '''
+class LinearRegression(Regression):
+    def __init__(self, n_iterations=100, learning_rate=0.001, gradient_descent=True):
+        self.gradient_descent = gradient_descent
+        super(LinearRegression, self).__init__(n_iterations=n_iterations,
+                                            learning_rate=learning_rate)
+    def fit(self, X, y):
+        # If not gradient descent => Least squares approximation of w
+        if not self.gradient_descent:
+            '''
             β = (X^T X)^-1 X^T y
             Estimates both the intercept and all coefficients.
-        '''
-        X_T = X.transpose()
+            '''
+            X_T = X.transpose()
 
-        inversed = np.linalg.inv( X_T.dot(X) )
-        coefficients = inversed.dot(X_T).dot(y)
-
-        return coefficients
+            inversed = np.linalg.inv( X_T.dot(X) )
+            self.weights = inversed.dot(X_T).dot(y)
+            print(self.weights)
+        else:
+            super(LinearRegression, self).fit(X, y)
---
+++ miniml/regression/__init__.py
@@ -0,0 +1,3 @@
+from .MultiLinearRegression import LinearRegression
+
+__all__ = ["LinearRegression"]
